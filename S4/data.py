83f8399ee41b9698ad267766befff11188ab42b4
Tristan Torchet <torchet.tristan@gmail.com>
2024-09-11 01:39:53 +0200

    S4D - vanilla benchmarking on cifar and mnist

--- S4/data.py
+++ S4/data.py
@@ -4,12 +4,16 @@ import torch
 from jax import numpy as jnp
 from torch.utils.data import TensorDataset
 from tqdm import tqdm
+import torchvision
+import torchvision.transforms as transforms
 
 
-def create_sin_x_dataset(n_examples=1024, bsz=128):
+
+def create_sin_x_dataset(bsz=128):
     print("[*] Generating Toy Dataset: sin(x)...")
 
     # Constants
+    N_SAMPLES = 1024
     SEQ_LENGTH, N_CLASSES, IN_DIM = 16, 8, 1
     x = np.linspace(0, 2 * np.pi, num=SEQ_LENGTH)
     # jnp.digitize: Return the indices of the bins to which each value in input array belongs.
@@ -19,7 +23,7 @@ def create_sin_x_dataset(n_examples=1024, bsz=128):
     # data will be a tensor of shape (n_examples, SEQ_LENGTH, IN_DIM)
     data = torch.Tensor(
         np.tile(
-            np.expand_dims(np.expand_dims(y, -1), 0), reps=[n_examples, 1, 1]
+            np.expand_dims(np.expand_dims(y, -1), 0), reps=[N_SAMPLES, 1, 1]
         )
     )
 
@@ -38,7 +42,7 @@ def create_sin_x_dataset(n_examples=1024, bsz=128):
     return trainloader, testloader, N_CLASSES, SEQ_LENGTH, IN_DIM, data
 
 
-def create_sin_ax_b_dataset(n_examples=2000, bsz=256): # TODO: n_examples=20000 (in original code)
+def create_sin_ax_b_dataset(bsz=256): # TODO: n_examples=20000 (in original code)
     '''
     Generate a dataset of sin(ax + b) functions.
     :param n_examples: number of examples to generate
@@ -47,15 +51,16 @@ def create_sin_ax_b_dataset(n_examples=2000, bsz=256): # TODO: n_examples=20000 
     '''
     print("[*] Generating sin(ax + b) Dataset...")
 
+    N_SAMPLES = 1024
     # Constants – `a` sampled uniform from [1, 10], `b` sampled uniform [0, 5]
     SEQ_LENGTH, N_CLASSES, IN_DIM, A_MAX, B_MAX = 128, 8, 1, 10, 5 # TODO SEQ_LENGTH=16000 in original code
     train_data, test_data = [], []
     data_key = jax.random.PRNGKey(21)
 
     # Loop through `n_examples` and generate data
-    print(f"\t=>> Generating {n_examples} Training Examples...")
+    print(f"\t=>> Generating {N_SAMPLES} Training Examples...")
     x = np.linspace(0, 2 * np.pi, num=SEQ_LENGTH)
-    for _ in tqdm(range(n_examples)):
+    for _ in tqdm(range(N_SAMPLES)):
         data_key, a_rng, b_rng = jax.random.split(data_key, num=3)
 
         # Compute a, b
@@ -94,3 +99,117 @@ def create_sin_ax_b_dataset(n_examples=2000, bsz=256): # TODO: n_examples=20000 
     )
 
     return trainloader, testloader, N_CLASSES, SEQ_LENGTH, IN_DIM, (jnp.array(train_data), jnp.array(test_data))
+
+def create_mnist_dataset(bsz=128):
+    print("[*] Generating MNIST Sequence Modeling Dataset...")
+
+    # Constants
+    SEQ_LENGTH, N_CLASSES, IN_DIM = 784, 256, 1
+
+    tx = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Lambda(
+                lambda x: (x.view(IN_DIM, SEQ_LENGTH).t() * 255).int()
+            ),
+        ]
+    )
+
+    train = torchvision.datasets.MNIST(
+        "./data", train=True, download=True, transform=tx
+    )
+    test = torchvision.datasets.MNIST(
+        "./data", train=False, download=True, transform=tx
+    )
+
+    # Return data loaders, with the provided batch size
+    trainloader = torch.utils.data.DataLoader(
+        train,
+        batch_size=bsz,
+        shuffle=True,
+    )
+    testloader = torch.utils.data.DataLoader(
+        test,
+        batch_size=bsz,
+        shuffle=False,
+    )
+
+    return trainloader, testloader, N_CLASSES, SEQ_LENGTH, IN_DIM
+
+def create_mnist_classification_dataset(bsz=128):
+    print("[*] Generating MNIST Classification Dataset...")
+
+    # Constants
+    SEQ_LENGTH, N_CLASSES, IN_DIM = 784, 10, 1
+    tf = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=0.5, std=0.5),
+            transforms.Lambda(lambda x: x.view(IN_DIM, SEQ_LENGTH).t()),
+        ]
+    )
+
+    train = torchvision.datasets.MNIST(
+        "./data", train=True, download=True, transform=tf
+    )
+    test = torchvision.datasets.MNIST(
+        "./data", train=False, download=True, transform=tf
+    )
+
+    # Return data loaders, with the provided batch size
+    trainloader = torch.utils.data.DataLoader(
+        train, batch_size=bsz, shuffle=True
+    )
+    testloader = torch.utils.data.DataLoader(
+        test, batch_size=bsz, shuffle=False
+    )
+
+    return trainloader, testloader, N_CLASSES, SEQ_LENGTH, IN_DIM
+
+
+def create_cifar_classification_dataset(bsz=128):
+    print("[*] Generating CIFAR-10 Classification Dataset")
+
+    # Constants
+    SEQ_LENGTH, N_CLASSES, IN_DIM = 32 * 32, 10, 3
+    tf = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+            ),
+            transforms.Lambda(lambda x: x.view(IN_DIM, SEQ_LENGTH).t()),
+        ]
+    )
+
+    train = torchvision.datasets.CIFAR10(
+        "./data", train=True, download=True, transform=tf
+    )
+    test = torchvision.datasets.CIFAR10(
+        "./data", train=False, download=True, transform=tf
+    )
+
+    # Return data loaders, with the provided batch size
+    trainloader = torch.utils.data.DataLoader(
+        train, batch_size=bsz, shuffle=True
+    )
+    testloader = torch.utils.data.DataLoader(
+        test, batch_size=bsz, shuffle=False
+    )
+
+    return trainloader, testloader, N_CLASSES, SEQ_LENGTH, IN_DIM
+
+
+Datasets = {
+    "mnist": create_mnist_dataset,
+    # "quickdraw": create_quickdraw_dataset,
+    # "fsdd": create_fsdd_dataset,
+    # "sc": create_sc_dataset,
+    "sin": create_sin_x_dataset,
+    "sin_noise": create_sin_ax_b_dataset,
+    "mnist-classification": create_mnist_classification_dataset,
+    # "fsdd-classification": create_fsdd_classification_dataset,
+    "cifar-classification": create_cifar_classification_dataset,
+    # "imdb-classification": create_imdb_classification_dataset,
+    # "listops-classification": create_listops_classification_dataset,
+}
